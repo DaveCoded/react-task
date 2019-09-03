179cb7ba2881bf6e0fcada37219a01448e2c4a40
David Bernhard <davidbernhard@live.com>
2019-09-03 12:29:21 +0100

    add number validation to myID

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,33 @@
 import React from 'react';
 
 const Header = ({ changeFunc, inputVal }) => {
+  const legend =
+    (inputVal && inputVal < 1) || (inputVal && inputVal > 10) ? (
+      <legend>Please choose a number between 1 and 10</legend>
+    ) : null;
+
   return (
     <header>
       <div style={{ position: 'relative' }}>
         <h1>Posts Page</h1>
-        <label
-          htmlFor='my-id'
-          style={{ position: 'absolute', left: '-70px', padding: '4px' }}>
-          My ID:
-        </label>
-        <input
-          className='my-id'
-          name='my-id'
-          type='number'
-          min='1'
-          max='10'
-          placeholder='Please select between 1 and 10'
-          value={inputVal}
-          onChange={changeFunc}
-        />
+        <fieldset>
+          <label
+            htmlFor='my-id'
+            style={{ position: 'absolute', left: '-70px', padding: '4px' }}>
+            My ID:
+          </label>
+          <input
+            className='my-id'
+            name='my-id'
+            type='number'
+            min='1'
+            max='10'
+            placeholder='Please select between 1 and 10'
+            value={inputVal}
+            onChange={changeFunc}
+          />
+          {legend}
+        </fieldset>
       </div>
     </header>
   );
